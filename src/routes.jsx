74c3f42c0ea0a4ba365ef3ce9e8af019f98069fc
heartsantor <antorboss.bd@gmail.com>
2024-07-05 18:18:20 +0600

    make setup

---
+++ src/routes.jsx
@@ -0,0 +1,39 @@
+import React, { Suspense, Fragment, lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Loader from './components/Loader/Loader';
+import { BASE_URL } from './config/constant';
+
+export const renderRoutes = (routes = []) => (
+  <Suspense fallback={<Loader />}>
+    <Routes>
+      {routes.map((route, i) => {
+        const Guard = route.guard || Fragment;
+        const Layout = route.layout || Fragment;
+        const Element = route.element;
+
+        return (
+          <Route
+            key={i}
+            path={route.path}
+            element={
+              <Guard>
+                <Layout>{route.routes ? renderRoutes(route.routes) : <Element props={true} />}</Layout>
+              </Guard>
+            }
+          />
+        );
+      })}
+    </Routes>
+  </Suspense>
+);
+
+const routes = [
+  {
+    exact: 'true',
+    path: '/',
+    element: lazy(() => import('./views/auth/signin/SignIn'))
+  }
+];
+
+export default routes;
