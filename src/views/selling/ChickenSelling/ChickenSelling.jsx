17ba2295b0db8fe32744cd08abb35608afbf1764
heartsantor <antorboss.bd@gmail.com>
2024-07-10 21:58:15 +0600

    add

--- src/views/selling/ChickenSelling/ChickenSelling.jsx
+++ src/views/selling/ChickenSelling/ChickenSelling.jsx
@@ -98,6 +98,81 @@ const ChickenSelling = () => {
             </Row>
             <Button variant="primary">ADD</Button>
             <hr />
+            <table>
+              <thead>
+                <tr>
+                  <th>SL</th>
+                  <th>Product Name</th>
+                  <th>Product QTY</th>
+                  <th>Product Unit</th>
+                  <th>Product price</th>
+                  <th>Product Total price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Chicken</td>
+                  <td>10</td>
+                  <td>50KG</td>
+                  <td>150</td>
+                  <td>1500</td>
+                  <td>X remove</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Chicken</td>
+                  <td>10</td>
+                  <td>50KG</td>
+                  <td>150</td>
+                  <td>1500</td>
+                  <td>X remove</td>
+                </tr>
+              </tbody>
+            </table>
+            <div>
+              <Row>
+                <Col md={6}>
+                  <Row>
+                    <Col md={12}>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Discount</Form.Label>
+                        <Form.Control type="text" placeholder="Discount" />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col md={6}>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Total price</td>
+                        <td>3000</td>
+                      </tr>
+                      <tr>
+                        <td>Total discount</td>
+                        <td>100</td>
+                      </tr>
+                      <tr>
+                        <td>after total</td>
+                        <td>2900</td>
+                      </tr>
+                      <tr>
+                        <td>given</td>
+                        <td>2000</td>
+                      </tr>
+                      <tr>
+                        <td>due</td>
+                        <td>900</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </Col>
+              </Row>
+              <Button variant="primary">Submit</Button>
+              <Button variant="secondary">Reset</Button>
+            </div>
           </Form>
         </Card.Body>
       </Card>
